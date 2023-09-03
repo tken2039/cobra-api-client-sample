0b227f9e261e0b0b201a86b97457b2c1a4127473
tken2039 <tken2039@gmail.com>
2023-09-03 18:32:00 +0900

    Fix package name

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/spf13/cobra"
-	apiclientcmd "github.com/tken2039/cobra-api-client-sample/cmd/apicallcmd"
+	"github.com/tken2039/cobra-api-client-sample/cmd/apicallcmd"
 )
 
 func main() {
@@ -19,7 +19,7 @@ func rootCmd() *cobra.Command {
 		Short: "This tool is a sample to create a pseudo API client using the Go CLI framework cobra.",
 	}
 
-	rootCmd.AddCommand(apiclientcmd.APICallCmd())
+	rootCmd.AddCommand(apicallcmd.APICallCmd())
 
 	return rootCmd
 }
